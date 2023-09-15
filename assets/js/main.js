f274e5a70668b2204027cc4b5b7a9fcfeab0462d
lorenzo-neri <lorenzo21neri@gmail.com>
2023-09-15 15:13:30 +0200

    fix bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,17 @@ Milestone 1
 Milestone 2
 ● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
 ● Click sul contatto mostra la conversazione del contatto cliccato
+
+Milestone 3
+● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+
+Milestone 4
+● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+
+Milestone 5 - Bonus opzionale
+● Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
+● Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti
 */
 
 const { createApp } = Vue
